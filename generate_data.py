8c9bc6afd4fc0bc6db30af2f7aa62e8975949a72
davcki <113711790+davcki@users.noreply.github.com>
2022-09-23 11:05:10 +0200

    Delete generate_data.py

--- generate_data.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-# Fixing random state for reproducibility
-np.random.seed(19680801)
-
-mu, sigma = 100, 15
-x_gaussian = mu + sigma * np.random.randn(10000)
-
-np.savetxt('file_data.csv', x_gaussian, delimiter=',')
